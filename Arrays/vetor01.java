d3f894ac754f06831160adec61265b458f66c9be
Guid0Dev <102263909+Guid0Dev@users.noreply.github.com>
2025-02-12 18:49:16 +0000

    Aprendi a trabalhar com dois vetores usando for.

---
+++ Arrays/vetor01.java
@@ -0,0 +1,13 @@
+package vetor01;
+
+public class vetor01{
+
+    public static void main(String[] args){
+
+        String mes[] = {janeiro, fevereiro, março, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro}
+        String total[] = {31, 28, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31}
+
+        for(i=0; i<mes[].lenth)
+        }
+    }
+}
