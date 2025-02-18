de5608b8014b5b1e457586801ccdb3ae56491c54
Guid0Dev <102263909+Guid0Dev@users.noreply.github.com>
2025-02-18 12:00:54 +0000

    Novos Exercicios

---
+++ Arrays/repetidores.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class repetidores {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int x = 0;
+
+        while(x>=10){
+            system.out.print("Contagem regressiva: " + 0);
+            int cont = x+1;
+        }
+        sc.close();
+    }
+
+}
--- Arrays/vetor01.java
+++ Arrays/vetor01.java
@@ -4,10 +4,11 @@ public class vetor01{
 
     public static void main(String[] args){
 
-        String mes[] = {janeiro, fevereiro, março, abril, maio, junho, julho, agosto, setembro, outubro, novembro, dezembro}
-        String total[] = {31, 28, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31}
+        String mes[] = {"janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"};
+        String total[] = {"31", "28", "31", "30", "31", "31", "30", "31", "30", "31", "30", "31" };
 
-        for(i=0; i<mes[].lenth)
+        for(int i=0; i< mes.length; i++){
+            System.out.println("O mes " + mes[i] + " tem " + total[i] + " dias.");
+        }
         }
     }
-}
