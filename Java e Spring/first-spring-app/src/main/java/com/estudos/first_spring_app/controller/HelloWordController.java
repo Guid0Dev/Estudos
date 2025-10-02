f2d1b272408f122d4b8fd342fdac5ac80b9e42d3
Guilherme Souza <guilhermeval58@gmail.com>
2025-10-02 18:40:03 -0300

    Organizando as pastas

---
+++ Java e Spring/first-spring-app/src/main/java/com/estudos/first_spring_app/controller/HelloWordController.java
@@ -0,0 +1,15 @@
+package com.estudos.first_spring_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+
+public class HelloWordController {
+    @GetMapping    
+    public String helloWord(){
+        return "Hello Video!";
+    }
+}
