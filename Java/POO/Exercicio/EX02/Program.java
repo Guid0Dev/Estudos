3c94cbb3971b4a637458f507f9ef45e334760457
Guilherme Souza <guilhermeval58@gmail.com>
2026-01-21 08:39:15 -0300

    POO EX2 pt2

--- Java/POO/Exercicio/EX02/Program.java
+++ Java/POO/Exercicio/EX02/Program.java
@@ -7,6 +7,18 @@ public class Program {
         v[1] = new Video("Aula 01 de Herança");
         v[2] = new Video("Aula 01 de Polimorfismo");
 
+        Gafanhoto[] g = new Gafanhoto[2];
+        g[0] = new Gafanhoto("Jubileu", 22, "M", 15);
+
         System.out.println(v[0].toString());
+
+        Visualizacao vis[] = new Visualizacao[5];
+        Visualizacao vi = new Visualizacao(g[0], v[2]);
+        vis[0].avaliar();
+
+
+        vis[1] = new Visualizacao(g[0], v[1]);
+        vis[1].avaliar(87.0f);
+
     }
 }
