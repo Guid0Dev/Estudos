d33b187e0a7c546a0a9dcededee3675d3501ce85
Guilherme Souza <guilhermeval58@gmail.com>
2026-01-21 00:44:23 -0300

    Agregação de classes

--- Java/POO/RelacionamentoClasses/Luta.java
+++ Java/POO/RelacionamentoClasses/Luta.java
@@ -1,15 +1,83 @@
 package POO.RelacionamentoClasses;
 
+import java.util.Random;
+
 public class Luta {
-    private String desafiado;
-    private String desafiante;
+    private Lutador desafiado;
+    private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
 
-    public void marcarLuta(){
-
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if(l1.getCategoria() == l2.getCategoria()
+        && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        } else{
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
     }
-    public void luta(){
+    public void lutar(){
+        if(this.aprovada){
+            System.out.println("### DESAFIADO ###");
+            this.desafiado.apresentar();
+            System.out.println("### DESAFIANTE ###");
+            this.desafiante.apresentar();
+
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3);
+            switch (vencedor) {
+                case 0:
+                    System.out.println("Empatou!");
+                    this.desafiante.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println("Desafiante " + this.desafiante.getNome() + " ganhou!");
+                    this.desafiante.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println("Desafiado " + this.desafiado.getNome() + " ganhou!");
+                    this.desafiante.perderLuta();
+                    this.desafiante.ganharLuta();
+                    break;
+                default:
+                    break;
+            }
+        } else{
+            System.out.println("A luta não pode ocorrer!");
+        }
         
     }
+
+    // Métodos Especiais//
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+    public int getRounds() {
+        return rounds;
+    }
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+    public boolean getAprovada() {
+        return aprovada;
+    }
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    
 }
