477bb1f5cd4ca3c972d334363bea36a7681ca438
Guilherme Souza <guilhermeval58@gmail.com>
2026-02-18 18:45:48 -0300

    Data e hora

---
+++ Java/Horario/Atual/Program.java
@@ -0,0 +1,31 @@
+package Horario.Atual;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Program {
+    public static void main(String[] args){
+
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        Instant d07 = Instant.parse("2022-07-20T01:30:26-3:00");
+        LocalDate d08 = LocalDate.parse("20/07/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+        LocalDate.of(2026, 7, 20);
+
+        System.out.println(d01);
+        System.out.println(d02);
+        System.out.println(d03);
+        System.out.println(d04);
+        System.out.println(d05);
+        System.out.println(d06);
+        System.out.println(d07);
+        System.out.println(d08.toString());
+    }
+}
