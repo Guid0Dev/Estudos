a061784e82081848f540ad1720d917b60cac2bc1
Guilherme Souza <guilhermeval58@gmail.com>
2026-02-25 23:33:45 -0300

    Enumerações

---
+++ Java/Enumeracoes/Ex01/entities/Order.java
@@ -0,0 +1,51 @@
+package Enumeracoes.Ex01.entities;
+
+import java.util.Date;
+import Enumeracoes.Ex01.entities02.OrderStatus;
+
+public class Order {
+    
+    private Integer id;
+    private Date moment;
+    private OrderStatus status;
+
+    public Order(){
+    
+    }
+
+    public Order(Integer id, Date moment, Enumeracoes.Ex01.entities02.OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order [id=" + id + ", moment=" + moment + ", status=" + status + "]";
+    }
+    
+}
---
+++ Java/Enumeracoes/Ex01/entities02/OrderStatus.java
@@ -0,0 +1,8 @@
+package Enumeracoes.Ex01.entities02;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
