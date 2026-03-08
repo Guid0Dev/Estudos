0027a802f007f86524caf888e9e85d81b8d5c556
Guilherme Souza <guilhermeval58@gmail.com>
2026-03-07 22:32:47 -0300

    bad solution

--- Java/Tratamento/aula03/entities/Reservations.java
+++ Java/Tratamento/aula03/entities/Reservations.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
 
+
 public class Reservations {
     private Integer roomNumber;
     private Date checkIn;
@@ -38,9 +39,18 @@ public class Reservations {
         long diff = checkOut.getTime() - checkIn.getTime();
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
-    public void updateDates(java.util.Date checkIn2, java.util.Date checkOut2){
+    public String updateDates(java.util.Date checkIn2, java.util.Date checkOut2){
+
+        Date now = new Date(roomNumber);
+            if(checkIn2.before(now) || checkOut2.before(now)){
+            return "Error in reservation: Reservation dates for update must be future dates.";
+            }
+            if(!checkOut2.after(checkIn2)){
+                return "Error in reservation: Check-out date must be after check-in date.";
+            }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
+        return null;
     }
     @Override
     public String toString(){
