e3f2ac3d2e032b016554fb77da3a02c23d2263d5
Guilherme Souza <guilhermeval58@gmail.com>
2026-06-09 15:10:14 -0300

    Add: keywords practice

---
+++ Java/inter/aula03/src/Son.java
@@ -0,0 +1,12 @@
+public final class Son extends Mother{
+    private String favoriteToy;
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
+    }
+    
+}
